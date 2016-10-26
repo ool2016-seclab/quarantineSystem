8097002d6879b41d1f1c1a0dc01ccdc5ef2196f0
K.Shuji <koikeshuji.official@gmail.com>
2016-10-26 14:06:06 +0900

    no message

--- hook_in.py
+++ hook_in.py
@@ -25,60 +25,62 @@ class QsysTest(SimpleSwitch13):
         super(QsysTest, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
+    #コントローラにSWが接続される
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        logger.info("Simple_Switch13_features")
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+    #Packet_inのハンドラが呼ばれる
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        #パケットから送信元のIP・MAC・宛先のIP・MAC・dataを取得
         allowTransportFlag = False
         msg = ev.msg
         datapath = msg.datapath
         dpid = datapath.id
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        
+        #送信元MACと送信元SWのポートの対応関係を記録
         self.mac_to_port.setdefault(dpid, {})
-        
-        pkt = packet.Packet(msg.data)
-        
-        self.logger.info("packet-in {}".format(pkt))
-        
-        _eth = pkt.get_protocol(ethernet.ethernet)
+        _eth = pkt.get_protocol(ethernet.ethernet)[0]
         if not _eth:
             self.logger.info("Not Ether type")
             return
-        _arp = pkt.get_protocol(arp.arp)
-        if not _arp:
-            self.logger.info("Not ARP type")
-        else:
-            self.logger.info("Arp:{}".format(_arp))
-        _ipv4 = pkt.get_protocol(ipv4.ipv4)
-        if not _ipv4:
-            self.logger.info("Not IPv4")
-        else:
-            self.logger.info("IPv4:{}".format(_ipv4))
         #スイッチのポート
         in_port = msg.match['in_port'] 
-        #MACアドレス
-        self.logger.info("Eth:{}".format(_eth))
-        #IPv4アドレス
-        #ipv4_src = ipv4_addr[0].src
-        allowTransportFlag = True
-        pkt_json = json.dumps(ev.msg.to_jsondict(), ensure_ascii=True,
-                                  indent=3, sort_keys=True)
-        allowTransportFlag = self.send_qsys(pkt_json);#通信許可T/Fを返す
-        if not(allowTransportFlag):#False
+        #[swのid][MACAddr]のテーブルにSwitch input portを登録
+        self.mac_to_port[dpid][_eth.src] = in_port
+
+        pkt = packet.Packet(msg.data)
+        self.logger.info("packet-in {}".format(pkt))
+        
+        pkt_dict = {
+            'src':_eth.src,
+            'dst':_eth.dst,
+            'data':msg.data
+            }
+        pkt_json = json.dumps(pkt_dict, sort_keys=True)
+        
+        if not self.send_qsys(pkt_json):#通信許可T/Fを返す
             logger,info('Drop:{}'.format(pkt_json))
             return
         #Transport to dst
         #print('Transport:{}⇢{}'.format(packet.ipv4_src))
-        src = _eth.src
-        dst = _eth.dst
-        self.logger.info("in_port:{}".format(in_port))
-        self.logger.info("dpid:{}".format(dpid))
-        self.logger.info("mac_arc:{}".format(src))
-        self.logger.info("mac_to_port:{}".format(self.mac_to_port))
         self.logger.info('json:{}'.format(json.dumps(ev.msg.to_jsondict(), ensure_ascii=True,
                                   indent=3, sort_keys=True)))
-        #[swのid][MACAddr]のテーブルにSwitch input portを登録
-        self.mac_to_port[dpid][_eth.src] = in_port
         #該当するSWの中にMacAddrがあるか？
         if _eth.src in self.mac_to_port[dpid]:
             #Switch output portをテーブルから指定
@@ -95,11 +97,3 @@ class QsysTest(SimpleSwitch13):
     def send_qsys(self, pkt_json):
         self.logger.info("Qsys_in{}".format(json))
         return True#pktの到達許可
-
-
-"""if __name__ == '__main__':
-    import sys
-    sys.argv.append(__name__)
-    from ryu.cmd import manager
-    manager.main()
-"""
