2fa1beebf0bc43a4d3c08f421782851930b8cb60
K.Shuji <koikeshuji.official@gmail.com>
2016-11-14 13:41:09 +0900

    no message

--- hook_test.py
+++ hook_test.py
@@ -84,7 +84,7 @@ class QsysTest(SimpleSwitch13):
         #if self.__DEBUG_MODE__:
         self.logger.info("packet-in {}".format(pkt))
         #パケットのヘッダ情報を取得
-        (self.logger.info("protocol:{}".format(str))
+        (self.logger.info("protocol:{}".format(p))
                 for p in pkt.protocols if type(p) != str)
         header_list = self.logger.info(dict((p.protocol_name, p)
                            for p in pkt.protocols if type(p) != str))
