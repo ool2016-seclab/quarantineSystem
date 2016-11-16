7546bd2c8d525afd920c855e6eed6e5fdd337c23
K.Shuji <koikeshuji.official@gmail.com>
2016-11-16 15:23:32 +0900

    mininetの設定を記述

---
+++ mn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.node import RemoteController
+
+if __name__ == '__main__':
+
+    net = Mininet()
+
+    net.addController('c0',controller=RemoteController)
+    s1 = net.addSwitch('s1')
+    host = {'h1':'10.0.0.1',
+            'h2':'10.0.0.2',
+            'h3':'10.0.0.3',
+            }
+    for h, ip in host:
+        addlink(net.addHost(h, ip), s1)
+    net.start()
