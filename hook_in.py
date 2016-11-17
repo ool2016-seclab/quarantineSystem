7198dc9113de60c279ba37aefc6ee09db6afef4e
K.Shuji <koikeshuji.official@gmail.com>
2016-11-18 01:33:00 +0900

    no message

--- hook_in.py
+++ hook_in.py
@@ -186,9 +186,10 @@ class QsysTest(SimpleSwitch13):
             ip_to_mac = {v:k for k, v in self.mac_to_ipv4.items()}
             self.logger.info("ip_to_mac{}".format(ip_to_mac))
             for ip, mac in ip_to_mac.items():
+                self.logger.info("IP:{}".format(ip))
                 self.logger.info("Eval:{}".format(self.qsys.get_reliability_eval(ip)))
                 if QsysRelEval.LOW == self.qsys.get_reliability_eval(ip):
-                    if not self.mac_deny_list[mac]:
+                    if not mac in self.mac_deny_list:
                         for dp in self.datapathes:#dp[0]:dp,dp[1]:parser
                             match = dp[1].OFPMatch(eth_arc=eth)
                             actions = []#Drop
