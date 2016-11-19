f1d2a2d5f58e014d6975a9f5e267ae6049304d82
K.Shuji <koikeshuji.official@gmail.com>
2016-11-19 21:55:40 +0900

    mininetの起動スクリプト

--- mn.py
+++ mn.py
@@ -3,21 +3,24 @@
 import six
 from mininet.net import Mininet
 from mininet.cli import CLI
+from mininet.link import Link
 from mininet.node import RemoteController, OVSSwitch
+from mininet.topo import Topo
+
+
 
 if __name__ == '__main__':
-
-    net = Mininet(switch=OVSSwitch)
-    net.addController('c0',controller=RemoteController)
-
-    s1 = net.addSwitch('s1')
+    topo = Topo()
+    s1 = topo.addSwitch('s1')
     host = {'h1':'10.0.0.1',
             'h2':'10.0.0.2',
             'h3':'10.0.0.3',
             }
     for h, ip in host:
-        net.addLink(s1,net.addHost(h, ip))
-
+        _host = topo.addHost(h,{'ip':ip})
+        topo.addLink(s1, _host)
+    net = Mininet(switch=OVSSwitch, topo=topo)
+    net.addController('c0',controller=RemoteController)
     net.build()
     net.start()
     command = 'ovs-vsctl set Bridge %s protocols=%s' % (s1, ['OpenFlow13'])
