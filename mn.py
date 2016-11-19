d92dade134f76975dea91cd704b6da71d85dac7f
K.Shuji <koikeshuji.official@gmail.com>
2016-11-19 23:51:47 +0900

    おとなしくminiedit.py生成しました。

--- mn.py
+++ mn.py
@@ -1,32 +1,67 @@
 #!/usr/bin/python2
 
-import six
 from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
 from mininet.cli import CLI
-from mininet.link import Link
-from mininet.node import RemoteController, OVSSwitch, Host
-from mininet.topo import Topo
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
 
+def myNetwork():
 
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='192.168.0.0/16')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, dpid='1')
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch, dpid='2')
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='192.168.1.1/24', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='192.168.1.2/24', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='192.168.1.3/24', defaultRoute=None)
+    
+    h4 = net.addHost('h4', cls=Host, ip='192.168.2.1/24', defaultRoute=None)
+    h5 = net.addHost('h5', cls=Host, ip='192.168.2.2/24', defaultRoute=None)
+    h6 = net.addHost('h6', cls=Host, ip='192.168.2.3/24', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s2, h4)
+    net.addLink(s2, h5)
+    net.addLink(s2, h6)
+
+    net.addLink(s2, s1)
+
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s1)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([c0])
+    net.get('s2').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
 
 if __name__ == '__main__':
-    net = Mininet()             
-    s1 = net.addSwitch('s1')
-    c0 = net.addController( 'c0', controller=RemoteController)
-    host = {
-        'h1':'10.0.0.1',
-        'h2':'10.0.0.2',
-        'h3':'10.0.0.3',
-        }
-    for h, ip in host:
-        _host = Host(h)
-        _host.setIP(ip,24)
-        net.addLink(s1, h)
-    net.start()
-    
-    command = 'ovs-vsctl set Bridge %s protocols=%s' % (s1, ['OpenFlow13'])
-    print(s1.cmd(command.split(' ')))
-    
-    CLI( net )                                                                                                            
-    net.stop()  
-    
+    setLogLevel( 'info' )
+    myNetwork()
