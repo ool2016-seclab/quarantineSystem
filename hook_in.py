70f6fdf14f8e12c99578bbfe007c37d2c6957645
K.Shuji <koikeshuji.official@gmail.com>
2016-11-22 01:00:47 +0900

    sysTest: Exception occurred during handler processing. Backtrace from offending handler [_packet_in_handler] servicing event [EventOFPPacketIn] follows. Traceback (most recent call last):   File "/usr/local/lib/python3.4/dist-packages/ryu/base/app_manager.py", line 290, in _event_loop     handler(ev)   File "/home/seclab/quarantineSystem/hook_in.py", line 131, in _packet_in_handler     self._packet_in_ipv4(msg, pkt, qsys_pkt, dp)   File "/home/seclab/quarantineSystem/hook_in.py", line 164, in _packet_in_ipv4     ip = dpkt.ip.IP(eth.data)   File "/usr/local/lib/python3.4/dist-packages/dpkt-1.8.6.2-py3.4.egg/dpkt/ip.py", line 26, in __init__     super(IP, self).__init__(*args, **kwargs)   File "/usr/local/lib/python3.4/dist-packages/dpkt-1.8.6.2-py3.4.egg/dpkt/dpkt.py", line 86, in __init__     self.unpack(args[0])   File "/usr/local/lib/python3.4/dist-packages/dpkt-1.8.6.2-py3.4.egg/dpkt/ip.py", line 93, in unpack     dpkt.Packet.unpack(self, buf)   File "/usr/local/lib/python3.4/dist-packages/dpkt-1.8.6.2-py3.4.egg/dpkt/dpkt.py", line 176, in unpack     struct.unpack(self.__hdr_fmt__, buf[:self.__hdr_len__])):   File "/usr/local/lib/python3.4/dist-packages/dpkt-1.8.6.2-py3.4.egg/dpkt/dpkt.py", line 107, in __getitem__     return getattr(self, k) TypeError: getattr(): attribute name must be string

--- hook_in.py
+++ hook_in.py
@@ -158,8 +158,8 @@ class QsysTest(SimpleSwitch13):
         _tcp = pkt.get_protocol(TCP)
         if _tcp:
             pcap = self.pcap.write_pkt(msg.data)
-            self.logger.info("pcap:{}".format(pcap))
-            payload = DpktPcapFromBytes(pcap)
+            self.logger.info("pcap:{}".format(str(pcap)))
+            payload = DpktPcapFromBytes(str(pcap))
             eth = dpkt.ethernet.Ethernet(msg.data)
             ip = dpkt.ip.IP(eth.data)
             __tcp = dpkt.tcp.TCP(ip.data)
