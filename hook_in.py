6c2e864691c23ddafa6d6a55234f61c1b03fada5
K.Shuji <koikeshuji.official@gmail.com>
2016-11-22 02:27:49 +0900

    no message

--- hook_in.py
+++ hook_in.py
@@ -163,9 +163,9 @@ class QsysTest(SimpleSwitch13):
             f = open('tmp', 'rb')
             payload = dpkt.pcap.Reader(f)
             for t,k in payload:
-                eth = dpkt.ethernet.Ethernet(k)
-                ip = dpkt.ip.IP(eth.data)
-                __tcp = dpkt.tcp.TCP(ip.data)
+                eth = dpkt.ethernet.Ethernet(k).data
+                ip = dpkt.ip.IP(eth.data).data
+                __tcp = dpkt.tcp.TCP(ip.data).data
                 self.logger.info("payload:{}".formay(__tcp))
         self.logger.info("data:{}".format(msg.data))
         qsys_pkt.set_data(msg.data)
