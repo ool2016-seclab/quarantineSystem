af4791449dc2a72b46847795a93697c1dc61132d
K.Shuji <koikeshuji.official@gmail.com>
2016-11-24 14:20:34 +0900

    no message

--- hook_in.py
+++ hook_in.py
@@ -223,10 +223,11 @@ class QsysTest(SimpleSwitch13):
                 ip = eth.data
                 l4 = ip.data
                 if type(l4) == dpkt.icmp.ICMP:
-                    self._packet_in_icmp(msg, pkt, qsys_pkt, dp, l4.data)
+                    self._packet_in_icmp(msg, pkt, qsys_pkt, d-p, l4.data)
                     return
                 elif type(l4) == dpkt.tcp.TCP:
-                    self._packet_in_tcp(msg, pkt, qsys_pkt, dp, l4)
+                    self.logger.info("TCP*{}".format(dpkt.tcp.TCP(l4).data))
+                    self._packet_in_tcp(msg, pkt, qsys_pkt, dp, l4.data)
                     return
                 elif type(l4) == dpkt.udp.UDP:
                     self._packet_in_udp(msg, pkt, qsys_pkt, dp, l4.data)
@@ -241,7 +242,7 @@ class QsysTest(SimpleSwitch13):
     def _packet_in_tcp(self, msg, pkt, qsys_pkt, dp, payload):
         self.logger.info("tcp")
         qsys_pkt.set_data(msg.data)
-        self.logger.info("payload:{}".format(payload.data))
+        self.logger.info("payload:{}".format(payload))
         try:#getHTTP
             request = dpkt.http.Request(__tcp.data)
             url = http.headers['host'] + http.uri
