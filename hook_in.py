0767927a1e34a9ab0da7bca4951fe91c011021bd
K.Shuji <koikeshuji.official@gmail.com>
2016-11-30 04:25:58 +0900

    no message

--- hook_in.py
+++ hook_in.py
@@ -369,7 +369,7 @@ class QsysTest(SimpleSwitch13):
                                    data=icmp_pkt.data))
         p.serialize()
         datapath = dp.datapath
-"""        actions = [dp.parser.OFPActionOutput(ofproto_v1_3.OFPP_FLOOD)]
+        """actions = [dp.parser.OFPActionOutput(ofproto_v1_3.OFPP_FLOOD)]
         out = dp.parser.OFPPacketOut(
             datapath=dp.datapath, buffer_id=ofproto_v1_3.OFP_NO_BUFFER, in_port=dp.in_port,
             actions=actions, data=p.data)
